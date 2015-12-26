5ed03fe06229d18f0de11fb94bcf738afaef3f10
Redth <jondick@gmail.com>
2015-12-26 13:37:20 -0500

    Added tests

---
+++ HttpTwo.Tests/HttpTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Http;
+
+namespace HttpTwo.Tests
+{
+    [TestFixture]
+    public class HttpTests
+    {
+        NodeHttp2Runner node;
+
+        [SetUp]
+        public void Setup ()
+        {
+            node = new NodeHttp2Runner ();
+            node.LogHandler = (msg) => {
+                Console.WriteLine (msg);
+            };
+            node.StartServer ();
+            // Wait for the server to initialize
+            System.Threading.Thread.Sleep (2000);
+        }
+
+        [TearDown]
+        public void Teardown ()
+        {
+            node.StopServer ();
+        }
+
+        [Test]
+        public async void Get_Single_Html_Page ()
+        {
+            var http2MsgHandler = new Http2MessageHandler ();
+            var http = new HttpClient (http2MsgHandler);
+
+            var data = await http.GetStringAsync ("http://localhost:8999/index.html");
+
+            Assert.IsNotNullOrEmpty (data);
+            Assert.IsTrue (data.Contains ("Hello World"));
+        }
+    }
+}
+
---
+++ HttpTwo.Tests/NodeHttp2Runner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace HttpTwo.Tests
+{
+    public class NodeHttp2Runner
+    {
+        Process process;
+
+        public Action<string> LogHandler { get; set; }
+
+        public void StartServer ()
+        {
+            if (process != null && !process.HasExited)
+                return;
+            
+            var scriptPath = Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "..", "..", "node-http2", "example", "server.js");
+
+            process = new Process ();
+            process.StartInfo = new ProcessStartInfo ("/usr/local/bin/node", scriptPath);
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.EnvironmentVariables.Add ("HTTP2_PLAIN", "true");
+            process.StartInfo.EnvironmentVariables.Add ("HTTP2_LOG", "trace");
+            process.StartInfo.EnvironmentVariables.Add ("HTTP2_LOG_DATA", "1");
+
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            process.EnableRaisingEvents = true;
+
+
+            process.ErrorDataReceived += (sender, e) => {
+                if (LogHandler != null)
+                    LogHandler (e.Data);
+            };
+            process.OutputDataReceived += (sender, e) => {
+                if (LogHandler != null)
+                    LogHandler (e.Data);
+            };
+
+            process.Start ();
+            process.BeginOutputReadLine ();
+            process.BeginErrorReadLine ();
+        }
+
+        public void StopServer ()
+        {
+            if (process != null && process.HasExited) { 
+                process.Kill ();
+                process.Close ();
+                process = null;
+            }
+        }
+    }
+}
+
